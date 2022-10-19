3a49adae1bc3f87110b5f507933d690a25cfef39
DiegoBillares <dbillares@uxdivers.com>
2022-10-19 15:32:31 -0300

    Implemented auth service method

--- src/Chefs/Business/IUserService.cs
+++ src/Chefs/Business/IUserService.cs
@@ -4,6 +4,7 @@ namespace Chefs.Business;
 
 public interface IUserService
 {
+    ValueTask<User?> Auth(string email, CancellationToken ct);
     ValueTask<User> GetUser(CancellationToken ct);
     ValueTask<ChefApp> GetChefSettings(CancellationToken ct);
     Task SetCheffSettings(ChefApp chefSettings, CancellationToken ct);
--- src/Chefs/Business/UserService.cs
+++ src/Chefs/Business/UserService.cs
@@ -8,18 +8,40 @@ public class UserService : IUserService
 {
     private readonly IUserEndpoint _userEndpoint;
     private readonly IWritableOptions<ChefApp> _chefAppOptions;
+    private readonly IWritableOptions<AuthenticationOptions> _authenticationOptions;
+    private User? _user;
 
-    public UserService(IUserEndpoint userEndpoint, IWritableOptions<ChefApp> chefAppOptions)
+    public UserService(IUserEndpoint userEndpoint, 
+        IWritableOptions<ChefApp> chefAppOptions, 
+        IWritableOptions<AuthenticationOptions> authenticationOptions)
     {
         _userEndpoint = userEndpoint;
         _chefAppOptions = chefAppOptions;
+        _authenticationOptions = authenticationOptions;
+    }
+
+    public async ValueTask<User?> Auth(string email, CancellationToken ct)
+    {
+        var user = await _userEndpoint.GetUser(email, ct);
+        if (user != null)
+        {
+            _user = new User(user);
+            await _authenticationOptions.UpdateAsync(_ => new AuthenticationOptions()
+            {
+                UserName = email,
+                SaveCredentials = true
+            });
+
+            return _user;
+        }
+        return null;
     }
 
     public async ValueTask<ChefApp> GetChefSettings(CancellationToken ct) => _chefAppOptions.Value;
 
     public async ValueTask<User> GetUser(CancellationToken ct)
     {
-        var userData = await _userEndpoint.GetUser("james.bondi@gmail.com", ct);
+        var userData = await _userEndpoint.GetUser(_user!.Email!, ct);
         var user = new User(userData);
         return user;
     }
