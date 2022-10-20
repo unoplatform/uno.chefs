adba2dca6737c0f92f44a93e5dfb69bfae01afc8
DiegoBillares <dbillares@uxdivers.com>
2022-10-20 11:41:21 -0300

    SearchFilter class refactoring

--- src/Chefs/Business/Models/SearchFilter.cs
+++ src/Chefs/Business/Models/SearchFilter.cs
@@ -2,11 +2,10 @@
 
 namespace Chefs.Business;
 
-public record SearchFilter
-{
-    public OrganizeCategories? OrganizeCategories { get; init; }
-    public Times? Times { get; init; }
-    public Difficulties? Difficulty { get; init; }
-    public Category? Category { get; init; }
-    public string? TextFilter { get; init; }
-}
+public record SearchFilter(
+    OrganizeCategories? OrganizeCategories, 
+    Times? Times, 
+    Difficulties? Difficulty, 
+    Category? Category,
+    string? TextFilter
+);
