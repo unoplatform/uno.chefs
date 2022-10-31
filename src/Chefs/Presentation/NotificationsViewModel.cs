89136756b5db0305453e130380511170102ba628
DiegoBillares <dbillares@uxdivers.com>
2022-10-31 17:45:28 -0300

    Implemented notification, profile, settings view models

--- src/Chefs/Presentation/NotificationsViewModel.cs
+++ src/Chefs/Presentation/NotificationsViewModel.cs
@@ -4,12 +4,19 @@ namespace Chefs.Presentation;
 
 public partial class NotificationsViewModel
 {
-    INotificationService _notificationService;
+    private readonly INotificationService _notificationService;
+    private readonly INavigator _navigator;
 
-    public NotificationsViewModel(INotificationService notificationService) 
-    { 
+    public NotificationsViewModel(INavigator navigator, INotificationService notificationService) 
+    {
+        _navigator = navigator;
         _notificationService = notificationService;
     }
 
     IListFeed<Notification> Notifications => ListFeed<Notification>.Async(async ct => await _notificationService.GetAll(ct));
+
+    public async ValueTask DoExist(CancellationToken ct)
+    {
+        await _navigator.NavigateBackAsync(ct);
+    }
 }
