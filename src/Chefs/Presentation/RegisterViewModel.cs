aaafe5c930d6fd0107555eaf0d77574ddcc1b739
DiegoBillares <dbillares@uxdivers.com>
2022-11-01 18:45:18 -0300

    feat: Register page first version

--- src/Chefs/Presentation/RegisterViewModel.cs
+++ src/Chefs/Presentation/RegisterViewModel.cs
@@ -2,15 +2,4 @@
 
 public partial class RegisterViewModel
 {
-    private readonly INavigator _navigator;
-
-    public RegisterViewModel(INavigator navigator) 
-    {
-        _navigator = navigator;
-    }
-
-    public async ValueTask NavigateBack(CancellationToken ct)
-    {
-        await _navigator.NavigateBackAsync(this, cancellation: ct);
-    }
 }
