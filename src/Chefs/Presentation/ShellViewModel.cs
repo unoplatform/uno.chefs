4b6b4159df34b4dffd5154b09e89a30932e0c7ae
Diego Billares <dbillares@uxdivers.com>
2022-11-04 15:53:22 -0300

    fix: Fixed welcome page, progress in login page

--- src/Chefs/Presentation/ShellViewModel.cs
+++ src/Chefs/Presentation/ShellViewModel.cs
@@ -31,7 +31,7 @@ public class ShellViewModel
         }
         else
         {
-            await _navigator.NavigateForResultAsync<Credentials>(this, Qualifiers.ClearBackStack);
+            await _navigator.NavigateViewModelAsync<WelcomeViewModel>(this, Qualifiers.ClearBackStack);
         }
     }
 }
