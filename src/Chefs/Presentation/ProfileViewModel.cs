8603b48ad559169b25a7c2457fd1f516362597af
Diego Billares <dbillares@uxdivers.com>
2022-11-04 18:49:38 -0300

    fix: Fixed some mistakes, add user id predefined

--- src/Chefs/Presentation/ProfileViewModel.cs
+++ src/Chefs/Presentation/ProfileViewModel.cs
@@ -26,68 +26,33 @@ public partial class ProfileViewModel
         _user = user;
     }
 
-    public IState<bool> IsMyProfile => State<bool>.Value(this, () => _user is not null);
+    public IState<bool> IsMyProfile => State<bool>.Value(this, () => _user is null);
 
-    public IFeed<User> Profile => Feed<User>.Async(async ct => await Task.Run(() => new User(new UserData()
-    {
-        UrlProfileImage = "https://picsum.photos/60/60",
-        FullName = "James Bondi",
-        Description = "Passionate about food and life",
-        Followers = 450,
-        Following = 124
-    })));
+    public IFeed<User> Profile => Feed<User>.Async(async ct => _user ?? await _userService.GetCurrent(ct));
 
-    //public IFeed<User> Profile => Feed<User>.Async(async ct => _user ?? await _userService.GetCurrent(ct));
+    public IListFeed<Cookbook> Cookbooks => ListFeed<Cookbook>.Async(async ct => await _cookbookService.GetByUser(_user?.Id 
+        ?? (await _userService.GetCurrent(ct)).Id, ct));
 
-    //public IListFeed<Cookbook> Cookbooks => ListFeed<Cookbook>.Async(async ct => await _cookbookService.GetByUser(_user?.Id ?? new Guid(), ct));
+    public IListFeed<Recipe> Recipes => ListFeed<Recipe>.Async(async ct => await _recipeService.GetByUser(_user?.Id
+        ?? (await _userService.GetCurrent(ct)).Id, ct));
 
-    // public IListFeed<Recipe> Recipes => ListFeed<Recipe>.Async(async ct => await _recipeService.GetByUser(_user?.Id ?? new Guid(), ct));
-
-    private IImmutableList<Cookbook> GetCookbooksMock(CancellationToken ct) => (new List<Cookbook>()
-            {
-                new Cookbook(new CookbookData()
-                {
-                    Name = "BreakFast",
-                    PinsNumber = 3
-                }),
-                new Cookbook(new CookbookData()
-                {
-                    Name = "Easy",
-                    PinsNumber = 10
-                })
-            }).ToImmutableList();
-
-    public IListFeed<Cookbook> Cookbooks => ListFeed<Cookbook>.Async(async ct => await Task.Run(() => GetCookbooksMock(ct)));
-
-    private IImmutableList<Recipe> GetRecipesMock(CancellationToken ct) => (new List<Recipe>()
-            {
-                new Recipe(new RecipeData()
-                {
-                    ImageUrl = "https://picsum.photos/70/146",
-                    Name = "Grilled Tofu Sandwich",
-                    Calories = "268 kcal",
-                    CookTime = new TimeSpan(0, 10, 0),
-                })
-            }).ToImmutableList();
-
-    public IListFeed<Recipe> Recipes => ListFeed<Recipe>.Async(async ct => await Task.Run(() => GetRecipesMock(ct)));
-
-    public async ValueTask DoExist(CancellationToken ct)
+    public async ValueTask Exit(CancellationToken ct)
     {
         await _navigator.NavigateBackAsync(ct);
     }
 
-    public async ValueTask DoSettingsNavigation(CancellationToken ct)
+    public async ValueTask SettingsNavigation(CancellationToken ct)
     {
-        await _navigator.NavigateViewModelAsync<SettingsViewModel>(this, cancellation: ct);
+        var user = _user ?? (await _userService.GetCurrent(ct));
+        await _navigator.NavigateViewModelAsync<SettingsViewModel>(this, data: user, cancellation: ct);
     }
 
-    public async ValueTask DoRecipeNavigation(Recipe recipe, CancellationToken ct)
+    public async ValueTask RecipeNavigation(Recipe recipe, CancellationToken ct)
     {
         await _navigator.NavigateViewModelAsync<RecipeDetailsViewModel>(this, data: recipe, cancellation: ct);
     }
 
-    public async ValueTask DoCookbookNavigation(Cookbook cookbook, CancellationToken ct)
+    public async ValueTask CookbookNavigation(Cookbook cookbook, CancellationToken ct)
     {
         await _navigator.NavigateViewModelAsync<LiveCookingViewModel>(this, data: cookbook, cancellation: ct);
     }
