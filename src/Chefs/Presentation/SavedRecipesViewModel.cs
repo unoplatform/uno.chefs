56b38f4283d816ee86fe187f6b38985d0f68f1d9
Diego Billares <dbillares@uxdivers.com>
2022-11-07 15:40:03 -0300

    feat: progress on saved flow

--- src/Chefs/Presentation/SavedRecipesViewModel.cs
+++ src/Chefs/Presentation/SavedRecipesViewModel.cs
@@ -1,9 +1,25 @@
-﻿namespace Chefs.Presentation;
+﻿using Chefs.Business;
+using Windows.System;
+
+namespace Chefs.Presentation;
 
 public partial class SavedRecipesViewModel
 {
-    public SavedRecipesViewModel()
-    {
+    private readonly INavigator _navigator;
+    private readonly IRecipeService _recipeService;
+    private readonly ICookbookService _cookbookService;
 
+    public SavedRecipesViewModel(INavigator navigator, 
+                                 IRecipeService recipeService, 
+                                 ICookbookService cookbookService)
+    {
+        _navigator = navigator;
+        _recipeService = recipeService;
+        _cookbookService = 
+        _cookbookService = cookbookService;
     }
+
+    public IListFeed<Cookbook> Cookbooks => ListFeed<Cookbook>.Async(async ct => await _cookbookService.GetSaved(ct));
+
+    public IListFeed<Recipe> Recipes => ListFeed<Recipe>.Async(async ct => await _recipeService.GetSaved(ct));
 }
