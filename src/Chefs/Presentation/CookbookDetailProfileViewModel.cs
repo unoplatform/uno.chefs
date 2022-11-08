0ef733dda93248e70456e9d51a3521df9dd518cf
Diego Billares <dbillares@uxdivers.com>
2022-11-08 18:43:13 -0300

    chore: Progress on cookbook detail view

---
+++ src/Chefs/Presentation/CookbookDetailProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Chefs.Business;
+
+namespace Chefs.Presentation;
+
+public partial class CookbookDetailProfileViewModel
+{
+    public CookbookDetailProfileViewModel(Cookbook cookbook)
+    {
+        PinNumbers = State.Value(this, () => cookbook.PinsNumber);
+        Recipes = State.Value(this, () => cookbook.Recipes!).AsListFeed();
+    }
+
+    public IState<int> PinNumbers { get; }
+
+    public IListFeed<Recipe> Recipes { get; }
+
+    public async ValueTask CreateCookbookNavigation(Cookbook cookbook, CancellationToken ct)
+    {
+
+    }
+}
