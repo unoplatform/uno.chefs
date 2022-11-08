5ec4ee5f1d633ac77f3190e386306065c2534bad
Diego Billares <dbillares@uxdivers.com>
2022-11-08 18:43:13 -0300

    chore: Progress on cookbook detail view

---
+++ src/Chefs/Presentation/CookbookDetailSavedViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Chefs.Business;
+
+namespace Chefs.Presentation;
+
+public partial class CookbookDetailSavedViewModel
+{
+    public CookbookDetailSavedViewModel(Cookbook cookbook)
+    {
+        Cookbook = State.Value(this, () => cookbook);
+    }
+
+    public IFeed<Cookbook> Cookbook { get; }
+
+    public async ValueTask CreateCookbookNavigation(CancellationToken ct)
+    {
+
+    }
+}
