f16bb49887a5495ea20c9317d47cdef46ece6dee
Mauro <mcocaro@uxdivers.com>
2022-11-09 10:58:54 -0300

    feat: LiveCookingPage

--- src/Chefs/Data/Models/StepData.cs
+++ src/Chefs/Data/Models/StepData.cs
@@ -8,6 +8,6 @@ public class StepData
     public int Number { get; set; }
     public TimeSpan CookTime { get; set; }
     public IImmutableList<string>? Cookware { get; set; }
-    public IImmutableList<IngredientData>? Ingredients { get; set; }
+    public IImmutableList<string>? Ingredients { get; set; }
     public string? Description { get; set; }
 }
