15a3f5a06e16f0b7f5b5a1356f8dce742e35b166
Mauro <mcocaro@uxdivers.com>
2022-11-09 10:58:54 -0300

    feat: LiveCookingPage

--- src/Chefs/Presentation/RecipeDetailsViewModel.cs
+++ src/Chefs/Presentation/RecipeDetailsViewModel.cs
@@ -27,22 +27,22 @@ public partial class RecipeDetailsViewModel
     public IListFeed<Review> Reviews => ListFeed.Async(async ct => await _recipeService.GetReviews(_recipe.Id, ct));
     public IListFeed<Step> Steps => ListFeed.Async(async ct => await _recipeService.GetSteps(_recipe.Id, ct));
 
-    private async ValueTask LiveCooking(CancellationToken ct) =>
-        await _navigator.NavigateViewModelAsync<LiveCookingViewModel>(this, data: Steps);
-
-    private async ValueTask IngredientsNavigation(CancellationToken ct) =>
+    public async ValueTask LiveCooking(CancellationToken ct) =>
+        await _navigator.NavigateViewModelAsync<LiveCookingViewModel>(this, data: await Steps);
+    
+    public async ValueTask IngredientsNavigation(CancellationToken ct) =>
         await _navigator.NavigateViewModelAsync<IngredientsViewModel>(this, data: _recipe);
 
-    private async ValueTask Review(CancellationToken ct) =>
+    public async ValueTask Review(CancellationToken ct) =>
         await _navigator.NavigateViewModelAsync<ReviewsViewModel>(this, data: _recipe);
 
-    private async ValueTask Save(Recipe recipe, CancellationToken ct) =>
+    public async ValueTask Save(Recipe recipe, CancellationToken ct) =>
         await _recipeService.Save(recipe, ct);
 
-    private async ValueTask Share(CancellationToken ct) =>
+    public async ValueTask Share(CancellationToken ct) =>
         throw new NotImplementedException();
 
-    private async ValueTask GoBack(CancellationToken ct) =>
+    public async ValueTask GoBack(CancellationToken ct) =>
         await _navigator.GoBack(this);
 
 }
