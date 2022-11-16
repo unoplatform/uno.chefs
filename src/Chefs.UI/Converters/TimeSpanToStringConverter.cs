8a7399ffe4a3317a27182ba26fd765e50cff6a3a
Mauro <mcocaro@uxdivers.com>
2022-11-16 09:57:55 -0300

    feat: added converters and small desing tweaks

---
+++ src/Chefs.UI/Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace Chefs.Converters;
+
+public class TimeSpanToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is TimeSpan ts)
+        {
+            var ftm = string.Format("{0} {1}",
+                ts.Hours > 0 ? ts.ToString(@"%h' hour.'") : string.Empty,
+                ts.Minutes > 0 ? ts.ToString(@"%m' mins'") : string.Empty);
+
+            return ftm;
+
+        }
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
