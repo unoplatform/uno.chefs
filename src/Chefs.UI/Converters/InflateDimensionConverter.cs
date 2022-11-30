f00a440ca60a3ef6cab7b6ee4b2b554cc418e9d7
Mauro <mcocaro@uxdivers.com>
2022-11-29 22:54:12 -0300

    feat: tab bar style

---
+++ src/Chefs.UI/Converters/InflateDimensionConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chefs.Converters
+{
+    public class InflateDimensionConverter : IValueConverter
+    {
+        public double Inflation { get; set; }
+        public double DefaultValue { get; set; } = 0d;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (double.TryParse(value.ToString(), out var dimension))
+            {
+                var result = dimension + Inflation;
+                if (result >= 0)
+                {
+                    return result;
+                }
+            }
+
+            return DefaultValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
