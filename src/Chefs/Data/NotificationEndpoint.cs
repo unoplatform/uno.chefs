866f94b81ba2ac14993fd59b3b70103e817a577a
Mauro <mcocaro@uxdivers.com>
2022-12-07 09:30:14 -0300

    feat: notifications

--- src/Chefs/Data/NotificationEndpoint.cs
+++ src/Chefs/Data/NotificationEndpoint.cs
@@ -6,7 +6,7 @@ namespace Chefs.Data;
 
 public class NotificationEndpoint : INotificationEndpoint
 {
-    public const string NotifacationDataFile = "notifications.json";
+    public const string NotifacationDataFile = "Notifications.json";
 
     private readonly IStorage _dataService;
     private readonly ISerializer _serializer;
@@ -14,7 +14,11 @@ public class NotificationEndpoint : INotificationEndpoint
     public NotificationEndpoint(IStorage dataService, ISerializer serializer)
         => (_dataService, _serializer) = (dataService, serializer);
 
-    public async ValueTask<IImmutableList<NotificationData>> GetAll(CancellationToken ct) => await _dataService
-        .ReadPackageFileAsync<IImmutableList<NotificationData>>(_serializer, NotifacationDataFile) 
+    public async ValueTask<IImmutableList<NotificationData>> GetAll(CancellationToken ct)
+    {
+        var a = await _dataService
+        .ReadPackageFileAsync<IImmutableList<NotificationData>>(_serializer, NotifacationDataFile)
         ?? ImmutableList<NotificationData>.Empty;
+        return a;
+    }
 }
