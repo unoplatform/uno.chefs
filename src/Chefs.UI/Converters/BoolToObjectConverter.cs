96407d55a5c240ce09fe5e8b84efb0d01c427968
Mauro <mcocaro@uxdivers.com>
2022-12-07 09:30:14 -0300

    feat: notifications

---
+++ src/Chefs.UI/Converters/BoolToObjectConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable disable
+namespace Chefs.Converters
+{
+    public class BoolToObjectConverter : IValueConverter
+    {
+        public object TrueValue { get; set; }
+
+        public object FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language) => (bool)value ? TrueValue : FalseValue;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotSupportedException("Only one-way conversion is supported.");
+    }
+}
+#nullable enable
