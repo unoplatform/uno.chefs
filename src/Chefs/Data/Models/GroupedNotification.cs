24e5cee5fd91bc857cb0fe0908cff7ef3024c361
Mauro <mcocaro@uxdivers.com>
2022-12-16 15:00:04 -0300

    feat: notifications grouping

---
+++ src/Chefs/Data/Models/GroupedNotification.cs
@@ -0,0 +1,26 @@
+﻿using Chefs.Business;
+using System.Collections.Immutable;
+
+namespace Chefs.Data.Models;
+
+public partial record GroupedNotification
+{
+    private readonly IImmutableList<Notification> _all;
+
+    public GroupedNotification(IEnumerable<Notification> notifications)
+    {
+        _all = notifications.ToImmutableList();
+        Today = _all.Where(x => x.Date.IsSameDate(DateTime.Today)).ToImmutableList();
+        Yesterday = _all.Where(x => x.Date.IsSameDate(DateTime.Now.AddDays(-1))).ToImmutableList();
+        Older = _all.Where(x => x.Date < DateTime.Now.AddDays(-1)).ToImmutableList();
+    }
+
+    public IImmutableList<Notification> Today { get; }
+    public bool HasTodayNotifications => Today.Any();
+    public IImmutableList<Notification> Yesterday { get; }
+    public bool HasYesterdayNotifications => Yesterday.Any();
+    public IImmutableList<Notification> Older { get; }
+    public bool HasOlderNotifications => Older.Any();
+
+    public IImmutableList<Notification> GetAll() => _all;
+}
--- src/Chefs/Presentation/NotificationsModel.cs
+++ src/Chefs/Presentation/NotificationsModel.cs
@@ -1,27 +1,20 @@
 ﻿using Chefs.Business;
+using Chefs.Data.Models;
 
 namespace Chefs.Presentation;
 
-public partial class NotificationsModel // DR_REV: Use Model suffix instead of ViewModel
+public partial class NotificationsModel
 {
     private readonly INotificationService _notificationService;
-    private readonly INavigator _navigator;
 
-    public NotificationsModel(INavigator navigator, INotificationService notificationService) 
+    public NotificationsModel(INotificationService notificationService) 
     {
-        _navigator = navigator;
         _notificationService = notificationService;
     }
 
-    public IListFeed<Notification> Notifications => ListFeed<Notification>.Async(async ct => await _notificationService.GetAll(ct));
+    public IFeed<GroupedNotification> Notifications => Feed<GroupedNotification>.Async(async ct => new GroupedNotification(await _notificationService.GetAll(ct)));
 
-    public IListFeed<Notification> Unread => Notifications.Where(x => !x.Read);
+    public IFeed<GroupedNotification> Unread => Notifications.Select(x => new GroupedNotification(x.GetAll().Where(y => !y.Read)));
 
-    public IListFeed<Notification> Read => Notifications.Where(x => x.Read);
-
-    // DR_REV: Do not prefix public method with "Do"
-    public async ValueTask DoExist(CancellationToken ct)
-    {
-        await _navigator.NavigateBackAsync(ct);
-    }
+    public IFeed<GroupedNotification> Read => Notifications.Select(x => new GroupedNotification(x.GetAll().Where(y => y.Read)));
 }
