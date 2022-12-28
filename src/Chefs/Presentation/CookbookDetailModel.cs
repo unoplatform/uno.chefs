b484a86f730d5cf36c7433b9eb40b8523ea80abe
Diego Billares <dbillares@uxdivers.com>
2022-12-28 17:54:29 -0300

    fix: update cookbook list and details

--- src/Chefs/Presentation/CookbookDetailModel.cs
+++ src/Chefs/Presentation/CookbookDetailModel.cs
@@ -18,16 +18,16 @@ public partial class CookbookDetailModel
         _cookbook = cookbook;
     }
 
-    public IState<Cookbook> Cookbook => State<Cookbook>.Value(this, () => _cookbook ?? new Cookbook());
+    //public IState<Cookbook> Cookbook => State<Cookbook>.Value(this, () => _cookbook ?? new Cookbook());
 
     private IListState<Recipe> _recipes => ListState<Recipe>.Value(this, () => _cookbook?.Recipes ?? ImmutableList<Recipe>.Empty);
 
     public IListFeed<Recipe> Recipes => _recipes;
 
-    public async ValueTask CreateCookbookNavigation(Cookbook cookbook, CancellationToken ct)
+    public async ValueTask CreateCookbookNavigation(/*Cookbook cookbook,*/ CancellationToken ct)
     {
-		// DR_REV: You are supposed to be able to data-bind 2-way to the Cookbook.Value do nav in XAML only
-
+        // DR_REV: You are supposed to be able to data-bind 2-way to the Cookbook.Value do nav in XAML only
+        var cookbook = _cookbook;
 		var result = await _navigator.GetDataAsync<UpdateCookbookModel, Cookbook>(this, data: cookbook, cancellation: ct);
 
         if(result is not null)
--- src/Chefs/Presentation/UpdateCookbookModel.cs
+++ src/Chefs/Presentation/UpdateCookbookModel.cs
@@ -24,18 +24,21 @@ public partial class UpdateCookbookModel // DR_REV: Use Model suffix instead of 
         _cookbook = cookbook;
     }
 
-    public IState<Cookbook> Cookbook => State.Value(this, () => _cookbook);
+    //public IState<Cookbook> Cookbook => State.Value(this, () => _cookbook);
 
     public IListState<Recipe> Recipes => ListState.Async(this, async ct =>
     {
-        var cookbook = await Cookbook;
+        //var cookbook = await Cookbook;
+        var cookbook = _cookbook;
 
-		var recipes = await _recipeService.GetSaved(ct);
-		var recipesExceptCookbook = cookbook?.Recipes is null 
-			? recipes
-			: recipes.RemoveAll(r => cookbook.Recipes.Any(cbR => r.Id == cbR.Id));
+        //var recipes = await _recipeService.GetSaved(ct);
+        //var recipesExceptCookbook = cookbook?.Recipes is null 
+        //	? recipes
+        //	: recipes.RemoveAll(r => cookbook.Recipes.Any(cbR => r.Id == cbR.Id));
+        var recipesExceptCookbook = await _recipeService.GetAll(ct);
 
-		return recipesExceptCookbook;
+
+        return recipesExceptCookbook;
     });
 
     public async ValueTask Done(CancellationToken ct)
