9d993059148a99a5f41cfcfea1f176bb9d45f356
Diego Billares <dbillares@uxdivers.com>
2023-01-05 13:17:47 -0300

    fix: cookbook details reactive issue

--- src/Chefs/Presentation/CookbookDetailModel.cs
+++ src/Chefs/Presentation/CookbookDetailModel.cs
@@ -19,20 +19,4 @@ public partial class CookbookDetailModel
     }
 
     public IState<Cookbook> Cookbook => State<Cookbook>.Value(this, () => _cookbook ?? new Cookbook());
-
-    private IListState<Recipe> _recipes => ListState<Recipe>.Value(this, () => _cookbook?.Recipes ?? ImmutableList<Recipe>.Empty);
-
-    public IListFeed<Recipe> Recipes => _recipes;
-
-    public async ValueTask CreateCookbookNavigation(Cookbook cookbook, CancellationToken ct)
-    {
-        // DR_REV: You are supposed to be able to data-bind 2-way to the Cookbook.Value do nav in XAML only
-		var result = await _navigator.GetDataAsync<UpdateCookbookModel, Cookbook>(this, data: cookbook, cancellation: ct);
-
-        if(result is not null)
-        {
-            _cookbook = result;
-            //await Cookbook.Update(_ => _cookbook, ct);
-        }
-    }
 }
