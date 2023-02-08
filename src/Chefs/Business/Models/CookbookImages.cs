fcb6a91e27b589151ca6cc9e4f3576fd95e3df07
Diego Billares <dbillares@uxdivers.com>
2023-02-08 01:31:52 -0300

    feat: Multi-photo Cookbooks Cards#200 implementation

---
+++ src/Chefs/Business/Models/CookbookImages.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+using Chefs.Data;
+
+namespace Chefs.Business.Models;
+
+public class CookbookImages
+{
+    public CookbookImages(ImmutableList<RecipeData> recipesData)
+    {
+        FirstImage = recipesData[0].ImageUrl;
+        SecondImage = recipesData[1].ImageUrl;
+        ThirdImage = recipesData[2].ImageUrl;
+    }
+
+    public string? FirstImage { get; set; }
+
+    public string? SecondImage { get; set; }
+
+    public string? ThirdImage { get; set; }
+}
