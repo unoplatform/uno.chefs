b7c0afa96665c9679956ac7168707799acc41ddc
Diego Billares <dbillares@uxdivers.com>
2023-03-10 19:15:37 -0300

    fix: fonts sizes and popular creatores issues

---
+++ src/Chefs.UI/Converters/StringReplacerConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Data;
+namespace Chefs.Converters;
+
+public class StringReplacerConverter : IValueConverter
+{
+   public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is string)
+        {
+            string[] stringParams = ((string)parameter).Split(',');
+            if(stringParams.Length == 1) return ((string)value).Replace(stringParams[0], "\n");
+            return ((string)value).Replace(stringParams[0], stringParams[1]);
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
