1b2a1d7c156ee35a61468818e42195e3e68f08f0
Steve Bilogan <steve.bilogan@gmail.com>
2023-05-27 13:43:10 -0400

    fix(Release): Adjust LinkerConfig to properly deserialize ImmutableList for Mobile/WASM

--- src/Chefs.Mobile/Android/Main.Android.cs
+++ src/Chefs.Mobile/Android/Main.Android.cs
@@ -1,6 +1,7 @@
 using Android.Runtime;
 using Com.Nostra13.Universalimageloader.Core;
 using Microsoft.UI.Xaml.Media;
+using System.Collections.Immutable;
 
 namespace Chefs.Droid
 {
@@ -17,6 +18,14 @@ namespace Chefs.Droid
 			: base(() => new AppHead(), javaReference, transfer)
 		{
 			ConfigureUniversalImageLoader();
+
+			// LinkDescription commented for now in the Chefs.Mobile.csproj as it is failing the Android Build
+			// Workaround in place here for System.Collections.Immutable.ImmutableList
+			var unused = ImmutableList.Create<string>();
+			var unused2 = ImmutableList.CreateBuilder<string>();
+			var unused3 = ImmutableList.CreateRange<string>(new string[] { "a", "b" });
+			var unused4 = ImmutableList.Create("a");
+			var unused5 = ImmutableList.Create(new string[] { "a", "b" });
 		}
 
 		private static void ConfigureUniversalImageLoader()
