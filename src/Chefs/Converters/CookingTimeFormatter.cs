d4ab2c866181b43ed4d7675bc251656dda119eb9
Marc-Antoine Soucy <ma.soucy@nventive.com>
2023-09-06 09:33:54 -0400

    fix: pr comments and small things

--- src/Chefs/Converters/CookingTimeFormatter.cs
+++ src/Chefs/Converters/CookingTimeFormatter.cs
@@ -7,15 +7,13 @@ public class CookingTimeFormatter : IValueConverter
 {
     public object? Convert(object value, Type targetType, object parameter, string language)
     {
-        if (value.GetType() == typeof(Time)) {
-            string timeString = (value.ToString() ?? Time.Under15min.ToString()).Replace("Under", "");
+        if (value is Time time) {
+            string timeString = (time.ToString() ?? Time.Under15min.ToString()).Replace("Under", "");
 
             return timeString.Insert(2, " ");
         }
-        else
-        {
-            return null;
-        }
+
+        return null;
     }
 
     public object? ConvertBack(object value, Type targetType, object parameter, string language)
