1a0385da61f4b35040377b1534a3cbfdf5615ca1
Lester Botello <lunarian.software@outlook.com>
2023-10-02 13:33:40 -0400

    Checkpoint

---
+++ src/Chefs/Business/Models/ChartModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore;
+
+namespace Chefs.Business.Models
+{
+    public class ChartModel : ObservableObject
+	{
+		public IEnumerable<ISeries> Series { get; set; } =
+			new[] { 2, 4, 1, 4, 3 }.AsPieSeries((value, series) =>
+		{
+			series.InnerRadius = 50;
+		});
+	}
+}
