613ec68c6b73b3bfc024e98708a284b2d678e1be
eriklimakc <erik.lima@knowcode.app>
2023-10-02 19:05:24 +0100

    chore: Fixing Empty content and navigation back

--- src/Chefs/Presentation/NotificationsModel.cs
+++ src/Chefs/Presentation/NotificationsModel.cs
@@ -1,11 +1,13 @@
 ﻿namespace Chefs.Presentation;
 public partial class NotificationsModel
 {
+	private readonly INavigator _navigator;
 	private readonly INotificationService _notificationService;
 
-	public NotificationsModel(INotificationService notificationService)
+	public NotificationsModel(INavigator navigator, INotificationService notificationService)
 	{
 		_notificationService = notificationService;
+		_navigator = navigator;
 	}
 	public IFeed<GroupedNotification> Notifications => Feed<GroupedNotification>.Async(async ct
 			=> await _notificationService.GetAll(ct) is { Count: > 0 } result
@@ -21,4 +23,14 @@ public partial class NotificationsModel
 			=> await _notificationService.GetRead(ct) is { Count: > 0 } result
 			? new GroupedNotification(result.Where(x => x.Read))
 			: Option.None<GroupedNotification>());
+
+	public async Task CloseNotificationPage()
+	{
+		await NavigateBack();
+	}
+
+	private async Task NavigateBack()
+	{
+		await _navigator.NavigateBackAsync(this);
+	}
 }
