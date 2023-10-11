2d7cdf9f2e450761b3df67396fa3dc67f5f34761
Steve Bilogan <steve.bilogan@gmail.com>
2023-10-10 20:46:28 -0400

    fix: touch up ui

--- src/Chefs/Presentation/FavoriteRecipesModel.cs
+++ src/Chefs/Presentation/FavoriteRecipesModel.cs
@@ -20,4 +20,23 @@ public partial class FavoriteRecipesModel
 
 	public IListFeed<Recipe> SavedRecipes => _recipeService.SavedRecipes;
 
+	public async Task ShowCurrentProfile()
+	{
+		var response = await _navigator.NavigateRouteForResultAsync<IChefEntity>(this, "Profile");
+		var result = await response!.Result;
+
+		await (result.SomeOrDefault() switch
+		{
+			UpdateCookbook updateCookbook => _navigator.NavigateViewModelAsync<CreateUpdateCookbookModel>(this, data: updateCookbook.Cookbook),
+			Cookbook cookbook when cookbook.Id == Guid.Empty => _navigator.NavigateViewModelAsync<CreateUpdateCookbookModel>(this),
+			Cookbook cookbook => _navigator.NavigateViewModelAsync<CookbookDetailModel>(this, data: cookbook),
+			object obj when obj is not null && obj.GetType() != typeof(object) => _navigator.NavigateDataAsync(this, obj),
+			_ => Task.CompletedTask,
+		});
+	}
+
+	public async Task ShowNotifications()
+	{
+		_ = _navigator.NavigateViewAsync<NotificationsContentPage>(this, qualifier: Qualifiers.Dialog);
+	}
 }
