1b989a4ec0200ec92413a63b037950c8f57056f2
Ariel De Los Santos <ariel.delossantos@nventive.com>
2023-11-17 08:24:58 -0500

    fix(tabNav): hidden button was shown on tab navigation

--- src/Chefs/Presentation/MainModel.cs
+++ src/Chefs/Presentation/MainModel.cs
@@ -2,36 +2,10 @@ namespace Chefs.Presentation;
 
 public partial class MainModel
 {
-	private readonly IUserService _userService;
-
 	public string? Title { get; }
 
-	private readonly INavigator _navigator;
-
-	public MainModel(
-		IUserService userService,
-		IOptions<AppConfig> appInfo,
-		INavigator navigator)
+	public MainModel(IOptions<AppConfig> appInfo)
 	{
 		Title = $"Main - {appInfo?.Value?.Title ?? "Chefs"}";
-		_navigator = navigator;
-		_userService = userService;
 	}
-
-	public async Task ShowProfile()
-	{
-		var response = await _navigator.NavigateRouteForResultAsync<IChefEntity>(this, "Profile", qualifier: Qualifiers.Dialog);
-		var result = await response!.Result;
-
-		await (result.SomeOrDefault() switch
-		{
-			UpdateCookbook updateCookbook => _navigator.NavigateViewModelAsync<CreateUpdateCookbookModel>(this, data: updateCookbook.Cookbook, qualifier: Qualifiers.Nested),
-			Cookbook cookbook when cookbook.Id == Guid.Empty => _navigator.NavigateViewModelAsync<CreateUpdateCookbookModel>(this, qualifier: Qualifiers.Nested),
-			Cookbook cookbook => _navigator.NavigateViewModelAsync<CookbookDetailModel>(this, data: cookbook, qualifier: Qualifiers.Nested),
-			object obj when obj is not null && obj.GetType() != typeof(object) => _navigator.NavigateDataAsync(this, obj, qualifier: Qualifiers.Nested),
-			_ => Task.CompletedTask,
-		});
-	}
-
-	public IFeed<User> UserProfile => _userService.UserFeed;
 }
