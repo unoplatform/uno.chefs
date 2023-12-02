be3936bb95ac54419b9825ae02829ab8d5072647
Steve Bilogan <steve.bilogan@gmail.com>
2023-12-02 10:59:56 -0500

    chore: refactor flyout logic

--- src/Chefs/Views/Flyouts/ResponsiveDrawerFlyout.xaml.cs
+++ src/Chefs/Views/Flyouts/ResponsiveDrawerFlyout.xaml.cs
@@ -1,41 +1,49 @@
+using Microsoft.UI.Xaml.Media;
+using Uno.UI;
+using Uno.UI.Extensions;
+
 namespace Chefs.Views.Flyouts;
 
 public abstract partial class ResponsiveDrawerFlyout : Flyout
 {
 	protected abstract DrawerOpenDirection WideOpenDirection { get; }
 
-	private readonly DrawerFlyoutPresenter _presenter;
+	private FlyoutPresenter? _presenter;
 
 	public ResponsiveDrawerFlyout()
 	{
 		this.InitializeComponent();
-
-		_presenter = new DrawerFlyoutPresenter()
-		{
-			OpenDirection = DrawerOpenDirection.Up,
-			DrawerLength = new GridLength(1, GridUnitType.Star),
-			Content = MainLayout
-		};
 	}
 
 	private void OnOpening(object? sender, object e)
 	{
-		if (sender is Flyout flyout && flyout.XamlRoot is { } root)
+		if (_presenter is { } presenter 
+			&& sender is Flyout flyout 
+			&& flyout.XamlRoot is { } root
+			)
 		{
+			//// TODO: Use Responsive Helpers?
 			if (root.Size.Width > 700)
 			{
-				_presenter.OpenDirection = WideOpenDirection;
-				_presenter.DrawerLength = new GridLength(0.66, GridUnitType.Star);
-				MainLayout.CornerRadius = GetWideCornerRadius();
+				DrawerFlyoutPresenter.SetOpenDirection(presenter, WideOpenDirection);
+				presenter.CornerRadius = GetWideCornerRadius();
 			}
 			else
 			{
-				_presenter.IsGestureEnabled = false;
+				DrawerFlyoutPresenter.SetDrawerLength(presenter, new GridLength(1, GridUnitType.Star));
+				DrawerFlyoutPresenter.SetIsGestureEnabled(presenter, false);
 			}
 		}
 	}
 
-	protected override Control CreatePresenter() => _presenter;
+	protected override Control CreatePresenter()
+	{
+		var basePresenter = base.CreatePresenter();
+
+		_presenter = basePresenter as FlyoutPresenter;
+
+		return basePresenter;
+	}
 
 	private CornerRadius GetWideCornerRadius()
 	{
