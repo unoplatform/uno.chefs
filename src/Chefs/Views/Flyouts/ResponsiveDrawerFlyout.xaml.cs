3bf25c35c90fd35aaf37de268cd8115d356755dd
Steve Bilogan <steve.bilogan@gmail.com>
2023-12-20 23:33:31 -0500

    chore: format whitespace

--- src/Chefs/Views/Flyouts/ResponsiveDrawerFlyout.xaml.cs
+++ src/Chefs/Views/Flyouts/ResponsiveDrawerFlyout.xaml.cs
@@ -1,7 +1,7 @@
 namespace Chefs.Views.Flyouts;
 
 public abstract partial class ResponsiveDrawerFlyout : Flyout
-{ 
+{
 	protected abstract DrawerOpenDirection WideOpenDirection { get; }
 
 	private FlyoutPresenter? _presenter;
