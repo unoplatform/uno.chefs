e3e478e7a12db611fd587e8a7d752c20010befcb
Steve Bilogan <steve.bilogan@gmail.com>
2024-01-12 11:02:38 -0500

    wip

--- src/Chefs/Views/Flyouts/ResponsiveDrawerFlyout.xaml.cs
+++ src/Chefs/Views/Flyouts/ResponsiveDrawerFlyout.xaml.cs
@@ -4,50 +4,10 @@ public abstract partial class ResponsiveDrawerFlyout : Flyout
 {
 	protected abstract DrawerOpenDirection WideOpenDirection { get; }
 
-	private FlyoutPresenter? _presenter;
+	//private FlyoutPresenter? _presenter;
 
 	public ResponsiveDrawerFlyout()
 	{
 		this.InitializeComponent();
-	}
-
-	private void OnOpening(object? sender, object e)
-	{
-		if (_presenter is { } presenter)
-		{
-
-			if ((this.XamlRoot?.Size.Width ?? 0d) > 700)
-			{
-				DrawerFlyoutPresenter.SetDrawerLength(presenter, new GridLength(0.66, GridUnitType.Star));
-				DrawerFlyoutPresenter.SetOpenDirection(presenter, WideOpenDirection);
-				presenter.CornerRadius = GetWideCornerRadius();
-			}
-			else
-			{
-				DrawerFlyoutPresenter.SetDrawerLength(presenter, new GridLength(1, GridUnitType.Star));
-				DrawerFlyoutPresenter.SetIsGestureEnabled(presenter, false);
-			}
-		}
-	}
-
-	protected override Control CreatePresenter()
-	{
-		var basePresenter = base.CreatePresenter();
-
-		_presenter = basePresenter as FlyoutPresenter;
-
-		return basePresenter;
-	}
-
-	private CornerRadius GetWideCornerRadius()
-	{
-		return WideOpenDirection switch
-		{
-			DrawerOpenDirection.Left => new CornerRadius(20, 0, 0, 20),
-			DrawerOpenDirection.Right => new CornerRadius(0, 20, 20, 0),
-			DrawerOpenDirection.Up => new CornerRadius(20, 20, 0, 0),
-			DrawerOpenDirection.Down => new CornerRadius(0, 0, 20, 20),
-			_ => throw new InvalidOperationException($"Invalid WideOpenDirection"),
-		};
 	}
 }
