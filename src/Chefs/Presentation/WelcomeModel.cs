1bbb5e48ad5d05a5e63cda899dfae87f589292da
Steve Bilogan <steve.bilogan@gmail.com>
2024-01-17 14:20:40 -0500

    chore: formatting

--- src/Chefs/Presentation/WelcomeModel.cs
+++ src/Chefs/Presentation/WelcomeModel.cs
@@ -15,7 +15,7 @@ public partial class WelcomeModel
 
 	public IFeed<bool> HasNext => NextPage.Select(x => x < PageCount - 1);
 
-	public async ValueTask Next(int nextPage, CancellationToken ct) 
+	public async ValueTask Next(int nextPage, CancellationToken ct)
 	{
 		if (nextPage >= 2)
 		{
