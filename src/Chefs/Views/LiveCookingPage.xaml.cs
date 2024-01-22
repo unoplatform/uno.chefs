c7c7d45ec72c6d37b522f7ca7365bde0414d6eb1
Steve Bilogan <steve.bilogan@gmail.com>
2024-01-22 13:58:18 -0500

    chore: fixes

--- src/Chefs/Views/LiveCookingPage.xaml.cs
+++ src/Chefs/Views/LiveCookingPage.xaml.cs
@@ -7,7 +7,5 @@ public sealed partial class LiveCookingPage : Page
 	public LiveCookingPage()
 	{
 		this.InitializeComponent();
-
-		mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Chefs/Assets/Videos/CookingVideo.mp4"));
 	}
 }
