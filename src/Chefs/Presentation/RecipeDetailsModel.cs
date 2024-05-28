35bf108f132203a3d357f7e33ca14e0cc1f72e36
Matthew Rajala <matthew.rajala@polymtl.ca>
2024-05-28 10:58:12 -0400

    trying out more share logic for windows
    trying out more share logic for windows

--- src/Chefs/Presentation/RecipeDetailsModel.cs
+++ src/Chefs/Presentation/RecipeDetailsModel.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.DataTransfer;
 
 namespace Chefs.Presentation;
@@ -57,14 +59,42 @@ public partial class RecipeDetailsModel
 	public async ValueTask Save(CancellationToken ct) =>
 		await _recipeService.Save(Recipe, ct);
 
+	[ComImport, Guid("3A3DCD6C-3EAB-43DC-BCDE-45671CE800C8")]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	interface IDataTransferManagerInterop
+	{
+		DataTransferManager GetForWindow([In] IntPtr appWindow, [In] ref Guid riid);
+		void ShowShareUIForWindow(IntPtr appWindow);
+	}
+
+	static readonly Guid _dtm_iid =
+		new Guid(0xa5caee9b, 0x8708, 0x49d1, 0x8d, 0x36, 0x67, 0xd2, 0x5a, 0x8d, 0xa0, 0x0c);
+
 	public async ValueTask Share(CancellationToken ct)
 	{
 #if WINDOWS
+		var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+		var DataTransferManagerInterop = (IDataTransferManagerInterop)WindowsRuntimeMarshal.GetActivationFactory(typeof(DataTransferManager));
 
+		var dtm = DataTransferManagerInterop.GetForWindow(hwnd, _dtm_iid);
+		//dtm.DataRequested += OnDataRequested;
+
+		//IntPtr dataTransferManagerPtr = interop.GetForWindow(hWnd, _dtm_iid);
+		//var dataTransferManager = Marshal.GetObjectForIUnknown(dataTransferManagerPtr) as DataTransferManager;
+
+		//dataTransferManager.DataRequested += (sender, args) =>
+		//{
+		//	args.Request.Data.Properties.Title = $"Sharing {Recipe.Name}";
+		//	args.Request.Data.Properties.Description = "Description";
+		//	args.Request.Data.SetText(Recipe.Details ?? "Details");
+		//	args.Request.Data.RequestedOperation = DataPackageOperation.Copy;
+		//};
+
+		//interop.ShowShareUIForWindow(hWnd);
 #else
-		var dataTransferManager = DataTransferManager.GetForCurrentView();
-		dataTransferManager.DataRequested += DataRequested;
-		DataTransferManager.ShowShareUI();
+        var dataTransferManager = DataTransferManager.GetForCurrentView();
+        dataTransferManager.DataRequested += DataRequested;
+        DataTransferManager.ShowShareUI();
 #endif
 	}
 
@@ -72,6 +102,6 @@ public partial class RecipeDetailsModel
 	{
 		args.Request.Data.Properties.Title = $"Sharing {Recipe.Name}";
 		args.Request.Data.Properties.Description = "Description";
-		args.Request.Data.SetText(Recipe.Details ?? "");
+		args.Request.Data.SetText(Recipe.Details ?? "Details");
 	}
 }
