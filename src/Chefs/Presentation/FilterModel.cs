c2f8c7924c157f5d7ee6693a8ea42702f2263544
Kunal22shah <kunalshah606@gmail.com>
2024-05-28 11:02:10 -0400

    chore: renaming property

--- src/Chefs/Presentation/FilterModel.cs
+++ src/Chefs/Presentation/FilterModel.cs
@@ -4,27 +4,27 @@ public partial class FilterModel
 {
 	private readonly INavigator _navigator;
 	private readonly IRecipeService _recipeService;
-
+	
 	public FilterModel(SearchFilter filters, INavigator navigator, IRecipeService recipeService)
 	{
 		_navigator = navigator;
 		_recipeService = recipeService;
-
+		
 		Filter = State.Value(this, () => filters);
 	}
-
+	
 	public IState<SearchFilter> Filter { get; }
 
-	public IEnumerable<FilterGroup> OrganizeCategories => Enum.GetValues(typeof(FilterGroup)).Cast<FilterGroup>();
+	public IEnumerable<FilterGroup> FilterGroups => Enum.GetValues(typeof(FilterGroup)).Cast<FilterGroup>();
 	public IEnumerable<Time> Times => Enum.GetValues(typeof(Time)).Cast<Time>();
 	public IEnumerable<Difficulty> Difficulties => Enum.GetValues(typeof(Difficulty)).Cast<Difficulty>();
 	public IEnumerable<int> Serves => new int[] { 1, 2, 3, 4, 5 };
-
+	
 	public IListFeed<Category> Categories => ListFeed.Async(_recipeService.GetCategories);
-
+	
 	public async ValueTask Search(SearchFilter filter, CancellationToken ct) =>
 		await _navigator.NavigateBackWithResultAsync(this, data: filter, cancellation: ct);
-
+	
 	public async ValueTask Reset(CancellationToken ct) =>
-	   await Filter.Update(current => new SearchFilter(), ct);
+		await Filter.Update(current => new SearchFilter(), ct);
 }
