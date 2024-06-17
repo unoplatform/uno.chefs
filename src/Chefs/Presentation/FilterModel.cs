802d0d9f54573665c8632881b23a607db31a5271
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2024-06-17 12:17:57 -0400

    chore: whitespaces

--- src/Chefs/Presentation/FilterModel.cs
+++ src/Chefs/Presentation/FilterModel.cs
@@ -4,22 +4,17 @@ public partial class FilterModel
 {
 	private readonly INavigator _navigator;
 	private readonly IRecipeService _recipeService;
-	
 	public FilterModel(SearchFilter filters, INavigator navigator, IRecipeService recipeService)
 	{
 		_navigator = navigator;
 		_recipeService = recipeService;
-		
 		Filter = State.Value(this, () => filters);
 	}
-	
 	public IState<SearchFilter> Filter { get; }
-
 	public IEnumerable<FilterGroup> FilterGroups => Enum.GetValues(typeof(FilterGroup)).Cast<FilterGroup>();
 	public IEnumerable<Time> Times => Enum.GetValues(typeof(Time)).Cast<Time>();
 	public IEnumerable<Difficulty> Difficulties => Enum.GetValues(typeof(Difficulty)).Cast<Difficulty>();
 	public IEnumerable<int> Serves => new int[] { 1, 2, 3, 4, 5 };
-	
 	public IListFeed<Category> Categories => ListFeed.Async(_recipeService.GetCategories);
 
 	public async ValueTask Search(SearchFilter filter) =>
