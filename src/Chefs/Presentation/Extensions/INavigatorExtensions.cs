21ecc7cb9e410bec63b0eda6d7b0cd5bc19d8191
Matthew Rajala <matthew.rajala@polymtl.ca>
2024-06-18 13:30:23 -0400

    chore: remove unecessary methods

--- src/Chefs/Presentation/Extensions/INavigatorExtensions.cs
+++ src/Chefs/Presentation/Extensions/INavigatorExtensions.cs
@@ -10,16 +10,6 @@ namespace Chefs.Presentation.Extensions;
 
 public static class INavigatorExtensions
 {
-	public static Task NavigateToProfile(this INavigator navigator, object sender, User? profile = null)
-	{
-		return navigator.NavigateRouteAsync(sender, "Profile", qualifier: Qualifiers.Dialog, data: profile);
-	}
-
-	public static Task NavigateToNotifications(this INavigator navigator, object sender)
-	{
-		return navigator.NavigateRouteAsync(sender, "Notifications", qualifier: Qualifiers.Dialog);
-	}
-
 	public static Task<NavigationResponse?> ShowDialog(this INavigator navigator, object sender, DialogInfo dialogInfo, CancellationToken ct)
 	{
 		return navigator.NavigateDataAsync(sender, new DialogInfo(dialogInfo.Title, dialogInfo.Content), cancellation: ct);
