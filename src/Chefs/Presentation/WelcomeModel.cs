0024e496cb470eb9760c102d5cefa16ab9ff8dd0
Steve Bilogan <steve.bilogan@gmail.com>
2024-06-24 21:30:20 -0400

    chore: cleanup welcomemodel

--- src/Chefs/Presentation/WelcomeModel.cs
+++ src/Chefs/Presentation/WelcomeModel.cs
@@ -3,12 +3,4 @@ namespace Chefs.Presentation;
 public partial record WelcomeModel(INavigator navigator)
 {
 	public IState<IntIterator> Pages => State<IntIterator>.Value(this, () => new IntIterator(Enumerable.Range(0, 3).ToImmutableList()));
-	
-	public async ValueTask NextPage(IntIterator pages)
-	{
-		if (!pages.CanMoveNext)
-		{
-			await navigator.NavigateViewModelAsync<LoginModel>(this, Qualifiers.ClearBackStack);
-		}
-	}
 }
