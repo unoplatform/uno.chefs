45a39f3cb480e2d873f4e8def38aef3822b1ff50
vatsashah45 <vatsashah45@gmail.com>
2024-09-12 14:03:21 -0400

    fix: Cookbook Details Navigation

--- src/Chefs/Presentation/CookbookDetailModel.cs
+++ src/Chefs/Presentation/CookbookDetailModel.cs
@@ -4,12 +4,15 @@ namespace Chefs.Presentation;
 public partial record CookbookDetailModel
 {
 	private readonly IMessenger _messenger;
+
+	private readonly Cookbook _cookbook;
 	public CookbookDetailModel(Cookbook cookbook, IMessenger messenger)
 	{
-		_messenger = messenger; 
+		_messenger = messenger;
+		_cookbook = cookbook ?? new Cookbook();
 	}
 	
 	public IState<Cookbook> Cookbook => State
-		.Value(this, () => new Cookbook())
+		.Value(this, () => _cookbook ?? new Cookbook())
 		.Observe(_messenger, cb => cb.Id);
 }
