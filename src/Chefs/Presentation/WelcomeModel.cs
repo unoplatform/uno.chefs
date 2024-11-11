6b503cba2fbea0163189db96cc4310a0c49e5efb
Kunal22shah <kunalshah606@gmail.com>
2024-11-11 10:24:00 -0500

    chore: PR comments

--- src/Chefs/Presentation/WelcomeModel.cs
+++ src/Chefs/Presentation/WelcomeModel.cs
@@ -1,6 +1,6 @@
 namespace Chefs.Presentation;
 
-public partial record WelcomeModel(INavigator Navigator)
+public partial record WelcomeModel()
 {
 	public IState<IntIterator> Pages => State<IntIterator>.Value(this, () => new IntIterator(Enumerable.Range(0, 3).ToImmutableList()));
 }
