8f7cc40b578c402c200d5f02c246d5bf0b9bce7f
Steve Bilogan <steve.bilogan@gmail.com>
2025-02-03 21:25:34 -0500

    chore: move to skia everywhere, net9.0

--- src/Chefs/Platforms/Android/Main.Android.cs
+++ src/Chefs/Platforms/Android/Main.Android.cs
@@ -1,6 +1,8 @@
 using System.Collections.Immutable;
 using Android.Runtime;
+#if !IS_ANDROID_SKIA
 using Com.Nostra13.Universalimageloader.Core;
+#endif
 using Microsoft.UI.Xaml.Media;
 
 namespace Chefs.Droid;
@@ -17,9 +19,12 @@ public class Application : Microsoft.UI.Xaml.NativeApplication
 	public Application(IntPtr javaReference, JniHandleOwnership transfer)
 		: base(() => new App(), javaReference, transfer)
 	{
+#if !IS_ANDROID_SKIA
 		ConfigureUniversalImageLoader();
+#endif
 	}
 
+#if !IS_ANDROID_SKIA
 	private static void ConfigureUniversalImageLoader()
 	{
 		// Create global configuration and initialize ImageLoader with this config
@@ -31,4 +36,5 @@ public class Application : Microsoft.UI.Xaml.NativeApplication
 
 		ImageSource.DefaultImageLoader = ImageLoader.Instance.LoadImageAsync;
 	}
+#endif
 }
--- src/Chefs/Platforms/WebAssembly/Program.cs
+++ src/Chefs/Platforms/WebAssembly/Program.cs
@@ -2,15 +2,23 @@ namespace Chefs;
 
 public class Program
 {
-	private static App? _app;
+	private static App _app;
 
-	public static int Main(string[] args)
-	{
-		Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/MaterialIcons-Regular.ttf#Material Symbols Outlined");
-		Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/FontAwesome-Brands.otf#Font Awesome 6 Brands");
+#if IS_WASM_SKIA
+		static async Task Main(string[] args)
+#else
+		static int Main(string[] args)
+#endif
+		{
+			Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/MaterialIcons-Regular.ttf#Material Symbols Outlined");
+			Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/FontAwesome-Brands.otf#Font Awesome 6 Brands");
 
-		Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
-
-		return 0;
-	}
+#if IS_WASM_SKIA
+			var host = new Uno.UI.Runtime.Skia.WebAssembly.Browser.PlatformHost(() => _app = new App());
+			await host.Run();
+#else
+			Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+			return 0;
+#endif
+		}
 }
