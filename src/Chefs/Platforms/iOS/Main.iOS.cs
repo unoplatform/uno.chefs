8f798132a6657df383cafaeb9cc93e6e6f603b3e
Jerome Laban <jerome@platform.uno>
2025-03-03 08:22:28 -0500

    chore: Adjust logging

--- src/Chefs/Platforms/iOS/Main.iOS.cs
+++ src/Chefs/Platforms/iOS/Main.iOS.cs
@@ -7,13 +7,15 @@ public class EntryPoint
 	// This is the main entry point of the application.
 	public static void Main(string[] args)
 	{
+		App.InitializeLogging();
+
 #if !IS_UIKIT_SKIA
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main(args, null, typeof(App));
+		// if you want to use a different Application Delegate class from "AppDelegate"
+		// you can specify it here.
+		UIApplication.Main(args, null, typeof(App));
 #else
-			var host = new Uno.UI.Runtime.Skia.AppleUIKit.PlatformHost(() => new App());
-			host.Run();
+		var host = new Uno.UI.Runtime.Skia.AppleUIKit.PlatformHost(() => new App());
+		host.Run();
 #endif
 	}
 }
