e3bf65a88b548d4c3ce1545782745b79b21cf2f1
Steve Bilogan <steve.bilogan@gmail.com>
2025-04-08 14:23:19 -0400

    chore: handle latest 6.0 changes

--- src/Chefs/Platforms/MacCatalyst/Main.maccatalyst.cs
+++ src/Chefs/Platforms/MacCatalyst/Main.maccatalyst.cs
@@ -12,7 +12,7 @@ public class EntryPoint
 			// you can specify it here.
 			UIApplication.Main(args, null, typeof(App));
 #else
-			var host = new Uno.UI.Runtime.Skia.AppleUIKit.PlatformHost(() => new App());
+			var host = new Uno.UI.Runtime.Skia.AppleUIKit.AppleUIKitHost(() => new App());
 			host.Run();
 #endif
 	}
