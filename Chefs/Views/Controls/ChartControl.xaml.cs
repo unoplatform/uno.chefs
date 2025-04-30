f49c1189caf40eae0f31c04a0271655357ddb30b
Steve Bilogan <steve.bilogan@gmail.com>
2025-04-30 14:35:08 -0400

    chore: make chartcontrol HD friendly

--- Chefs/Views/Controls/ChartControl.xaml.cs
+++ Chefs/Views/Controls/ChartControl.xaml.cs
@@ -1,33 +1,77 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using LiveChartsCore;
 using LiveChartsCore.ConditionalDraw;
 using LiveChartsCore.Measure;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using Microsoft.UI;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using SkiaSharp;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Chefs.Views.Controls;
 public sealed partial class ChartControl : UserControl
 {
 	private Recipe? _recipe;
+
+
+	public SolidColorBrush CarbBrush
+	{
+		get { return (SolidColorBrush)GetValue(CarbBrushProperty); }
+		set { SetValue(CarbBrushProperty, value); }
+	}
+
+	public static readonly DependencyProperty CarbBrushProperty =
+		DependencyProperty.Register("CarbBrush", typeof(SolidColorBrush), typeof(ChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Black), OnBrushChanged));
+
+	public SolidColorBrush ProteinBrush
+	{
+		get { return (SolidColorBrush)GetValue(ProteinBrushProperty); }
+		set { SetValue(ProteinBrushProperty, value); }
+	}
+
+	public static readonly DependencyProperty ProteinBrushProperty =
+		DependencyProperty.Register("ProteinBrush", typeof(SolidColorBrush), typeof(ChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Black), OnBrushChanged));
+
+	public SolidColorBrush FatBrush
+	{
+		get { return (SolidColorBrush)GetValue(FatBrushProperty); }
+		set { SetValue(FatBrushProperty, value); }
+	}
+
+	public static readonly DependencyProperty FatBrushProperty =
+		DependencyProperty.Register("FatBrush", typeof(SolidColorBrush), typeof(ChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Black), OnBrushChanged));
+
+
+
+	public SolidColorBrush DataLabelBrush
+	{
+		get { return (SolidColorBrush)GetValue(DataLabelBrushProperty); }
+		set { SetValue(DataLabelBrushProperty, value); }
+	}
+
+	public static readonly DependencyProperty DataLabelBrushProperty =
+		DependencyProperty.Register("DataLabelBrush", typeof(SolidColorBrush), typeof(ChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Black), OnBrushChanged));
+
+
+
+	public SolidColorBrush TrackBackgroundBrush
+	{
+		get { return (SolidColorBrush)GetValue(TrackBackgroundBrushProperty); }
+		set { SetValue(TrackBackgroundBrushProperty, value); }
+	}
+
+	public static readonly DependencyProperty TrackBackgroundBrushProperty =
+		DependencyProperty.Register("TrackBackgroundBrush", typeof(SolidColorBrush), typeof(ChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Black), OnBrushChanged));
+
+	private static void OnBrushChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+	{
+		if (dependencyObject is not ChartControl chartControl) return;
+
+		if (chartControl._recipe != null)
+		{
+			chartControl.BuildColumnChart();
+			chartControl.BuildDoughnutChart();
+		}
+	}
+
 	public ChartControl()
 	{
 		this.InitializeComponent();
@@ -66,7 +110,7 @@ public sealed partial class ChartControl : UserControl
 		var rowSeries = new RowSeries<NutritionChartItem>
 		{
 			Values = _chartdata,
-			DataLabelsPaint = new SolidColorPaint(GetSKColorFromResource("NutritionDataLabelColor")),
+			DataLabelsPaint = new SolidColorPaint(GetSKColorFromResource(DataLabelBrush)),
 			DataLabelsPosition = DataLabelsPosition.Right,
 			DataLabelsFormatter = point => $"{point.Model!.Name} {point.Model!.ChartProgressVal}/{point.Model!.MaxValueRef}g",
 			DataLabelsSize = 13,
@@ -96,7 +140,7 @@ public sealed partial class ChartControl : UserControl
 			IgnoresBarPosition = true,
 			MaxBarWidth = 22,
 			Padding = 1,
-			Fill = new SolidColorPaint(GetSKColorFromResource("NutritionTrackBackgroundColor"))
+			Fill = new SolidColorPaint(GetSKColorFromResource(TrackBackgroundBrush))
 		};
 		//End
 
@@ -136,20 +180,16 @@ public sealed partial class ChartControl : UserControl
 	{
 		return name switch
 		{
-			nameof(Nutrition.Carbs) => new SolidColorPaint(GetSKColorFromResource("NutritionCarbsValColor")),
-			nameof(Nutrition.Protein) => new SolidColorPaint(GetSKColorFromResource("NutritionProteinValColor")),
-			nameof(Nutrition.Fat) => new SolidColorPaint(GetSKColorFromResource("NutritionFatValColor")),
+			nameof(Nutrition.Carbs) => new SolidColorPaint(GetSKColorFromResource(CarbBrush)),
+			nameof(Nutrition.Protein) => new SolidColorPaint(GetSKColorFromResource(ProteinBrush)),
+			nameof(Nutrition.Fat) => new SolidColorPaint(GetSKColorFromResource(FatBrush)),
 			_ => new SolidColorPaint(SKColors.Yellow),
 		};
 	}
 
-	private SKColor GetSKColorFromResource(string resourceKey)
+	private SKColor GetSKColorFromResource(SolidColorBrush brush)
 	{
-		if (Resources.TryGetValue(resourceKey, out var resource) && resource is Color color)
-		{
-			return new SKColor(color.R, color.G, color.B, color.A);
-		}
-
-		return SKColor.Empty;
+		var color = brush.Color;
+		return new SKColor(color.R, color.G, color.B, color.A);
 	}
 }
