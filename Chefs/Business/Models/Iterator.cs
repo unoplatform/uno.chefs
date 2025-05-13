8aff0447201ba7610d03de5a14a3f041bacc2806
Matthew Rajala <matthew.rajala@polymtl.ca>
2025-05-13 13:37:54 -0400

    fix: welcome flipview selection states

--- Chefs/Business/Models/Iterator.cs
+++ Chefs/Business/Models/Iterator.cs
@@ -33,14 +33,4 @@ public record IntIterator(IImmutableList<int> Items)
 	public int Count => Items.Count;
 	public bool CanMoveNext => CurrentIndex < Items.Count - 1;
 	public bool CanMovePrevious => CurrentIndex > 0;
-
-	public IntIterator MoveNext()
-		=> CanMoveNext
-			? this with { CurrentIndex = CurrentIndex + 1 }
-			: this;
-
-	public IntIterator MovePrevious()
-		=> CanMovePrevious
-			? this with { CurrentIndex = CurrentIndex - 1 }
-			: this;
 }
