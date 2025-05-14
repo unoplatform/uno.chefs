66c84a0b92c7fdd21423c78c12c63359ba391525
Steve Bilogan <steve.bilogan@gmail.com>
2025-05-14 18:05:11 -0400

    fix: data loading

--- Chefs/Services/MockEndpoints/BaseMockEndpoint.cs
+++ Chefs/Services/MockEndpoints/BaseMockEndpoint.cs
@@ -3,7 +3,15 @@ namespace Chefs.Services.MockEndpoints;
 public abstract class BaseMockEndpoint(ISerializer serializer)
 {
 	protected T? LoadData<T>(string fileName)
-		=> serializer.FromString<T>(
-			File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "AppData", fileName))
-		);
+	{
+		var assembly = this.GetType().Assembly;
+		var resourceName = assembly.GetManifestResourceNames().FirstOrDefault(str => str.EndsWith(fileName));
+		if (resourceName == null)
+		{
+			throw new Exception($"Resource {fileName} not found in assembly {assembly.FullName}");
+		}
+		using var stream = assembly.GetManifestResourceStream(resourceName);
+		using var reader = new System.IO.StreamReader(stream);
+		return serializer.FromString<T>(reader.ReadToEnd());
+	}
 }
