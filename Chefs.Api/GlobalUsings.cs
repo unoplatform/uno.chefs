867bd058f01ed913e33475b9c231994bdf44238d
Kunal22shah <kunalshah606@gmail.com>
2025-05-14 10:00:55 -0400

    fix: global using

--- Chefs.Api/GlobalUsings.cs
+++ Chefs.Api/GlobalUsings.cs
@@ -2,4 +2,5 @@ global using System.Collections.Immutable;
 global using System.Text.Json;
 global using Chefs.Api.Entities;
 global using Chefs.Api.Serialization;
+global using System.Text.Json.Serialization;
 global using Microsoft.AspNetCore.Mvc;
