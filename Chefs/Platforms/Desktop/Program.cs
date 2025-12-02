fccd19c6c3a6a4da9faedc1eaba04beac55d52c5
Jonathan Pryor <jonpryor@vt.edu>
2025-12-02 12:14:29 -0500

    chore: enable logging on -desktop Platforms
    Context: https://github.com/unoplatform/uno.chefs/pull/1709
    
    It is extremely helpful to have log messages written to stdout
    when running on -desktop platforms such as macOS.
    
    Update `Desktop/Program.cs` to call `App.InitializeLogging()` so that
    messages are written to stdout.

--- Chefs/Platforms/Desktop/Program.cs
+++ Chefs/Platforms/Desktop/Program.cs
@@ -7,6 +7,8 @@ public class Program
     [STAThread]
     public static void Main(string[] args)
     {
+        App.InitializeLogging();
+
         var host = UnoPlatformHostBuilder.Create()
             .App(() => new App())
             .UseX11()
